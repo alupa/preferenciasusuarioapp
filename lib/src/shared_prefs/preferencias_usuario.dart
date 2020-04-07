da268e84136f89cd008faff729295715935c7a12
Alvaro Lupa <alvaro.lupa@gmail.com>
2020-04-07 19:25:31 -0400

    preferencias de usuariosoperativos

---
+++ lib/src/shared_prefs/preferencias_usuario.dart
@@ -0,0 +1,55 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenciasUsuario {
+
+  static final PreferenciasUsuario _instancia = new PreferenciasUsuario._internal();
+
+  factory PreferenciasUsuario() {
+    return _instancia;
+  }
+
+  PreferenciasUsuario._internal();
+
+  SharedPreferences _prefs;
+
+  initPrefs() async {
+    this._prefs = await SharedPreferences.getInstance();
+  }
+
+  // get y set del genero
+  get genero {
+    return _prefs.getInt('genero') ?? 1;
+  }
+
+  set genero (int value){
+    _prefs.setInt('genero', value);
+  }
+
+  // get y set del color secundario
+  get colorSecundario {
+    return _prefs.getBool('colorSecundario') ?? false;
+  }
+
+  set colorSecundario (bool value){
+    _prefs.setBool('colorSecundario', value);
+  }
+
+  // get y set del nombre usuario
+  get nombreUsuario {
+    return _prefs.getString('nombreUsuario') ?? '';
+  }
+
+  set nombreUsuario (String value){
+    _prefs.setString('nombreUsuario', value);
+  }
+
+  // get y set de la ultima pagina
+  get ultimaPagina {
+    return _prefs.getString('ultimaPagina') ?? 'home';
+  }
+
+  set ultimaPagina (String value){
+    _prefs.setString('ultimaPagina', value);
+  }
+
+}
